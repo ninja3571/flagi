7fad53a77a5f35eddb9761fa398a8cfdbb2447c1
ninja3571 <125036855+ninja3571@users.noreply.github.com>
2023-03-12 22:55:22 +0100

    3

--- app.js
+++ app.js
@@ -17,21 +17,106 @@ function los(){
 }
 
 function sto_eu(){
+    clearInterval
     const gra = document.getElementById("div3")
+    gra.classList.add("gra")
 
-    var kraj__eu = json[losuj()]
+    gra.innerHTML =""
 
-    const kraj_eu = document.createElement("div")
-    kraj_eu.classList.add("slide-in-fwd-center")
 
-    const nazwa_eu = document.createElement("h1")
-    const flag_eu = document.createElement("img")
+    if(kraj_eu.region=="Europe"){
+        var kraj_eu = json[losuj()]
 
-    nazwa_eu.innerHTML = kraj_eu.name
-    flag_eu.setAttribute("src",kraj_eu.flag)
+        const div_eu = document.createElement("div")
+        // kraj_eu.classList.add("slide-in-fwd-center")
+     
+         const nazwa_eu = document.createElement("h1")
+         const reg_eu = document.createElement("h2")
+         const flag_eu = document.createElement("img")
+     
+         nazwa_eu.innerHTML = kraj_eu.name
+         reg_eu.innerHTML = kraj_eu.region
+         flag_eu.setAttribute("src",kraj_eu.flag)
+         flag_eu.classList.add("flaga")
+     
+         gra.appendChild(div_eu)
+     
+         div_eu.appendChild(nazwa_eu)
+         div_eu.appendChild(flag_eu)
+         div_eu.appendChild(reg_eu)
+     
+         console.log(kraj_eu)
+    }
 
-    kraj_eu.appendChild(nazwa_eu)
-    kraj_eu.appendChild(flag_eu)
+    if(kraj_eu.region=="Asia"){
+        var kraj_as = json[losuj()]
+
+        const div_as = document.createElement("div")
+        // kraj_as.classList.add("slide-in-fwd-center")
+     
+         const nazwa_as = document.createElement("h1")
+         const reg_as = document.createElement("h2")
+         const flag_as = document.createElement("img")
+     
+         nazwa_as.innerHTML = kraj_as.name
+         reg_as.innerHTML = kraj_as.region
+         flag_as.setAttribute("src",kraj_as.flag)
+         flag_as.classList.add("flaga")
+     
+         gra.appendChild(div_as)
+     
+         div_as.appendChild(nazwa_as)
+         div_as.appendChild(flag_as)
+         div_as.appendChild(reg_as)
+     
+         console.log(kraj_as)
+    }
+    if(kraj_eu.region=="North America"){
+        var kraj_amn = json[losuj()]
+
+        const div_amn = document.createElement("div")
+        // kraj_amn.classList.add("slide-in-fwd-center")
+     
+         const nazwa_amn = document.createElement("h1")
+         const reg_amn = document.createElement("h2")
+         const flag_amn = document.createElement("img")
+     
+         nazwa_amn.innerHTML = kraj_amn.name
+         reg_amn.innerHTML = kraj_amn.region
+         flag_amn.setAttribute("src",kraj_amn.flag)
+         flag_amn.classList.add("flaga")
+     
+         gra.appendChild(div_amn)
+     
+         div_amn.appendChild(nazwa_amn)
+         div_amn.appendChild(flag_amn)
+         div_amn.appendChild(reg_amn)
+     
+         console.log(kraj_amn)
+    }
+    if(kraj_eu.region=="South America"){
+        var kraj_ams = json[losuj()]
+
+        const div_ams = document.createElement("div")
+        // kraj_amn.classList.add("slide-in-fwd-center")
+     
+         const nazwa_ams = document.createElement("h1")
+         const reg_ams = document.createElement("h2")
+         const flag_ams = document.createElement("img")
+     
+         nazwa_ams.innerHTML = kraj_ams.name
+         reg_ams.innerHTML = kraj_ams.region
+         flag_ams.setAttribute("src",kraj_ams.flag)
+         flag_ams.classList.add("flaga")
+     
+         gra.appendChild(div_ams)
+     
+         div_amn.appendChild(nazwa_ams)
+         div_amn.appendChild(flag_ams)
+         div_amn.appendChild(reg_ams)
+     
+         console.log(kraj_ams)
+    }
 
 }
 function serca(){
